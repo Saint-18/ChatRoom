00185aea0465ad5dee4f449d784b05fe251f7ca0
Adam Hough <hough.adam@gmail.com>
2024-09-21 16:57:21 -0500

    Added comments to main.py, removed unused functionality

--- main.py
+++ main.py
@@ -12,29 +12,39 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+# Route for the root of the web server
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
+    # Open the chatroom.html file
     with open("static/chatroom.html", "r") as f:
+        # Return the HTML content
         return HTMLResponse(content=f.read())
 
 
+# Route for other pages in the static directory
 @app.get("/{page}", response_class=HTMLResponse)
 async def read_root(page):
+    # Open the requested page
     with open(f"static/{page}", "r") as f:
+        # Return its contents as HTML
         return HTMLResponse(content=f.read())
 
 
+# API route to retrieve messages from the database
 @app.get("/api/messages/get/{chat_id}")
 def read_messages(chat_id: int):
     try:
         # Initialize database connection
         cnx = database.create_database_connection()
-        # Get results
+        # Request messages
         results = database.get_messages(cnx, chat_id)
+        # If no results returned
         if not results:
+            # Raise a 404 error
             raise HTTPException(
                 status_code=404, detail="No messages found in specified chat."
             )
+        # Return JSON results
         return results
 
     except Exception as e:
@@ -45,11 +55,8 @@ def read_messages(chat_id: int):
             database.close_database_connection(cnx)
 
 
+# TODO: Create a route to save messages to the database
 @app.post("/api/messages/create")
 def create_messages():
+    # Skip for now
     pass
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
