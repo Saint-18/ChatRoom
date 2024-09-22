c691ccb40f9f5bb797616c14013b42c019de5b57
Adam Hough <hough.adam@gmail.com>
2024-09-22 13:04:51 -0500

    Added API route for posting new messages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import database
 from typing import Union, List, Dict
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -55,8 +55,29 @@ def read_messages(chat_id: int):
             database.close_database_connection(cnx)
 
 
-# TODO: Create a route to save messages to the database
+# API route to post new messages to chat
 @app.post("/api/messages/create")
-def create_messages():
-    # Skip for now
-    pass
+async def create_messages(request: Request):
+    new_message = await request.json()
+    chat_id = new_message.get("chat_id")
+    message_body = new_message.get("message_body")
+    username = new_message.get("username")
+    try:
+        # Initialize database connection
+        cnx = database.create_database_connection()
+        # Insert new message in database
+        database.create_message(cnx, chat_id, message_body, username)
+        # If error inserting message
+        if not results:
+            # Raise a 404 error
+            raise HTTPException(status_code=404, detail="Unable to create new message.")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occured {str(e)}")
+    finally:
+        # Ensure the connection exists before attempting to close
+        if "cnx" in locals() and cnx:
+            database.close_database_connection(cnx)
+
+    # Return the updated messages for this chat
+    return read_messages(chat_id)
