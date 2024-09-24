98d34678366078dc4fac9a123daee8a0b6ba1edc
Adam Hough <hough.adam@gmail.com>
2024-09-24 11:24:19 -0500

    Added logic to handle user login

--- database.py
+++ database.py
@@ -43,6 +43,10 @@ def get_messages(cnx: object, chat_id: int):
     # dictionary=True returns results as JSON
     cursor = cnx.cursor(dictionary=True)
 
+    # TODO: Add message_id into JSON return
+    #       Insert message_id into HTML as ID tag for each message using JS
+    # TODO: Add role - need to show all messages if moderator, else only approved
+
     # Create the database query
     query = (
         "SELECT Users.username, Messages.message_body, Messages.created_at "
@@ -86,7 +90,52 @@ def create_message(cnx: object, chat_id: int, username: str, message_body: str):
         return False
 
 
-def close_database_connection(cnx):
+def get_login_details(cnx: object, username: str, password: str):
+    """
+    Retrieves all message for the requested chatroom
+    Input: cnx - MySQL database connection object, username - string username, password - string password
+    Returns: Array of JSON objects
+    """
+    # Create a cursor from the connection object (cnx)
+    cursor = cnx.cursor()
+
+    # Create the database query
+    query = (
+        # Looking for user_id matching supplied credentials
+        "SELECT Users.user_id "
+        "FROM Users "
+        "WHERE Users.username = %s AND Users.password_hash = %s;"
+    )
+
+    # Execute the query
+    cursor.execute(query, (username, password))
+    # Fetch results
+    user_id = cursor.fetchall()
+
+    # If the user_id is found, get list of approved chats and role
+    if user_id:
+        # Create a cursor from the connection object (cnx)
+        # dictionary=True returns results as JSON
+        cursor = cnx.cursor(dictionary=True)
+
+        # Create the database query
+        query = (
+            "SELECT Chats.chat_id, Chats.chat_title, Chat_Members.role "
+            "FROM Chats JOIN Chat_Members ON Chats.chat_id = Chat_Members.chat_id JOIN Users ON Users.user_id = Chat_Members.user_id "
+            "WHERE Users.username = %s;"
+        )
+
+        # Execute the query
+        cursor.execute(query, (username,))
+        # Fetch results
+        results = cursor.fetchall()
+
+        return results
+    else:
+        return None
+
+
+def close_database_connection(cnx: object):
     """
     Closes the connection to the database
     Input: cnx - MySQLConnection object
