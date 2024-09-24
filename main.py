85174971b3bda695f81f582a789232a495555c55
Adam Hough <hough.adam@gmail.com>
2024-09-24 11:24:40 -0500

    Added route handling for user login

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Union, List, Dict
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
@@ -11,12 +12,15 @@ app = FastAPI()
 # Serve static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Load templates
+templates = Jinja2Templates(directory="templates")
+
 
 # Route for the root of the web server
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     # Open the chatroom.html file
-    with open("static/chatroom.html", "r") as f:
+    with open("static/login.html", "r") as f:
         # Return the HTML content
         return HTMLResponse(content=f.read())
 
@@ -30,7 +34,16 @@ async def read_root(page):
         return HTMLResponse(content=f.read())
 
 
+# Route to handle dynamically loading chats using Jinja templating
+@app.get("/chat/{id}", response_class=HTMLResponse)
+async def display_chat_template(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="chat.html", context={"chat_id": id, "title": "Chat"}
+    )
+
+
 # API route to retrieve messages from the database
+# TODO: Convert to POST method to pass body with chat_id and role
 @app.get("/api/messages/get/{chat_id}")
 def read_messages(chat_id: int):
     try:
@@ -60,13 +73,13 @@ def read_messages(chat_id: int):
 async def create_messages(request: Request):
     new_message = await request.json()
     chat_id = new_message.get("chat_id")
-    message_body = new_message.get("message_body")
+    message_body = new_message.get("messageText")
     username = new_message.get("username")
     try:
         # Initialize database connection
         cnx = database.create_database_connection()
         # Insert new message in database
-        database.create_message(cnx, chat_id, message_body, username)
+        results = database.create_message(cnx, chat_id, username, message_body)
         # If error inserting message
         if not results:
             # Raise a 404 error
@@ -80,4 +93,31 @@ async def create_messages(request: Request):
             database.close_database_connection(cnx)
 
     # Return the updated messages for this chat
-    return read_messages(chat_id)
+    # return read_messages(chat_id)
+
+
+# Route to login
+@app.post("/login")
+async def handle_login_request(request: Request):
+    login_details = await request.json()
+    username = login_details.get("username")
+    password = login_details.get("password")
+    try:
+        # Initialize database connection
+        cnx = database.create_database_connection()
+        # Insert new message in database
+        results = database.get_login_details(cnx, username, password)
+        # If error inserting message
+        if not results:
+            # Raise a 403 error
+            raise HTTPException(
+                status_code=403, detail="Incorrect username or password."
+            )
+        return results
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occured {str(e)}")
+    finally:
+        # Ensure the connection exists before attempting to close
+        if "cnx" in locals() and cnx:
+            database.close_database_connection(cnx)
